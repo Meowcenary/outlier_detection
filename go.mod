7f48be55041b577fc2c789b05f9cb776e9d27adb
Eric Newman <newmaneric5@gmail.com>
2023-11-04 13:50:41 -0500

    Read Data With GoMNIST
    Read data with GoMNIST. Not really sure how to use it for anything
    yet

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.1
 
 require github.com/malaschitz/randomForest v0.0.0-20220708201519-82dce2f56816
 
-require gonum.org/v1/gonum v0.9.3 // indirect
+require (
+	github.com/petar/GoMNIST v0.0.0-20150320212226-2fbe10d0fa63 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
+)
--- main.go
+++ main.go
@@ -4,27 +4,24 @@ import (
 	// "fmt"
 	// "math/rand"
   //
-	// randomForest "github.com/malaschitz/randomForest"
-	"github.com/Meowcenary/outlier_detection/compressed_data_reader"
+	randomForest "github.com/malaschitz/randomForest"
+	"github.com/petar/GoMNIST"
 )
 
 func main() {
-	compressed_data_reader.Decompress("DERP")
-	// xData := [][]float64{}
-	// yData := []int{}
-  //
-	// for i := 0; i < 1000; i++ {
-	// 	x := []float64{rand.Float64(), rand.Float64(), rand.Float64(), rand.Float64()}
-	// 	y := int(x[0] + x[1] + x[2] + x[3])
-	// 	xData = append(xData, x)
-	// 	yData = append(yData, y)
-	// }
-  //
-	// forest := randomForest.Forest{}
-	// forest.Data = randomForest.ForestData{X: xData, Class: yData}
-	// forest.Train(1000)
-  //
-	// //test
-	// fmt.Println("Vote", forest.Vote([]float64{0.1, 0.1, 0.1, 0.1}))
-	// fmt.Println("Vote", forest.Vote([]float64{0.9, 0.9, 0.9, 0.9}))
+	// Read the MNIST data using GoMNIST
+	train, test, err := Load("./data")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: what data is there to even work with here?
+	// Outlier detection with isolated forest
+	xData := [][]float64{}
+	yData := []int{}
+	forest := randomForest.Forest{}
+	forest.Data = randomforest.ForestData{X: xData, Class: yData}
+	// replace 1000 with whatever number of values there are to compare
+	forest.Train(1000)
 }
